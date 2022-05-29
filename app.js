6934bfd7b2512b29efaa1a65aa3b8486aac919ad
Abhishek Singh <meabhisingh25@gmail.com>
2022-05-29 16:27:03 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import User from "./routers/User.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+export const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    useTempFiles: true,
+  })
+);
+app.use(cors());
+
+app.use("/api/v1", User);
